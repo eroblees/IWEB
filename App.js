2fecd8031249c0eedb1c325b1c3d411f28c422c4
Enrique <enrique.barra.arias@gmail.com>
2019-10-11 00:26:42 +0200

    update to expo 35

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { createStackNavigator, createAppContainer } from 'react-navigation';
+import { createAppContainer } from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
 
 import IndexScreen from './app/components/IndexScreen';
 import GameScreen from './app/components/GameScreen';
